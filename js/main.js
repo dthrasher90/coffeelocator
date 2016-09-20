061e51357292fbf4792341c634ac4e027590f76f
dthrasher90 <dthrasher90@GMAIL.COM>
2016-09-20 09:48:11 -0800

    json with map
    json with map

--- js/main.js
+++ js/main.js
@@ -3,16 +3,28 @@
 2) call jason
 3) parse json
 */
+var map;
 
-alert("linked");
+   function initMap() {
+     console.log("init map");
+    var map = new google.maps.Map(document.getElementById('map'), {
+       center: {lat: 64.8377778, lng: -147.71638889999997},
+       zoom: 11
+     });
+   }
+
+
+
 $(document).ready(function(){
-$.getJSON('https://api.foursquare.com/v2/venues/search?intent=browse&near=51.1506269,14.968706999999995&radius=19400&categoryId=4d4b7105d754a06374d81259&query=Restaurant&client_id=ITCEQQH3TV4ARRVUCNLK1P4TW1XOM3S1RKIZVPHFY452HFQO&client_secret=SOHNSC0TT4XBM31HVZR4FT13M5JLUONTWWSXE3INES5Y3LUO&v=20130307',
+$.getJSON('https://api.foursquare.com/v2/venues/search?intent=browse&near=64.8377778,-147.71638889999997&radius=19400&categoryId=4d4b7105d754a06374d81259&query=Coffee&client_id=ITCEQQH3TV4ARRVUCNLK1P4TW1XOM3S1RKIZVPHFY452HFQO&client_secret=SOHNSC0TT4XBM31HVZR4FT13M5JLUONTWWSXE3INES5Y3LUO&v=20130307',
+
     function(data) {
         $.each(data.response.venues, function(i,venues){
-            content = '<p>' + venues.name + '</p>';
+            content = '<p>' + venues.name +'</br>'+venues.location.address+ '</br>'+ venues.url+ '</br>' + venues.contact.formattedPhone+    '</p>';
             $(content).appendTo("#names");
+            console.log(data.response);
+          
        });
-});
-});
 
-alert("yep");
+});
+});
