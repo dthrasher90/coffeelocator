f737970f2a1ae5b6c2b4268e1fd94f01dd405c28
dthrasher90 <dthrasher90@GMAIL.COM>
2016-09-21 15:57:06 -0800

    working as of 9/21
    buttons working, redeisgn

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
-/*
 
-2) call jason
-3) parse json
-*/
 var map;
 
    function initMap() {
@@ -13,21 +9,32 @@ var map;
      });
    }
 
+   function getCityName(){
+     var city=$("#cityName").val();
+     console.log(city);
+     return;
+
+   }
+
 
 
 $(document).ready(function(){
-$.getJSON('https://api.foursquare.com/v2/venues/search?intent=browse&near=64.8377778,-147.71638889999997&radius=19400&categoryId=4d4b7105d754a06374d81259&query=Coffee&client_id=ITCEQQH3TV4ARRVUCNLK1P4TW1XOM3S1RKIZVPHFY452HFQO&client_secret=SOHNSC0TT4XBM31HVZR4FT13M5JLUONTWWSXE3INES5Y3LUO&v=20130307',
 
+$("button").click(function(){
+  //var cityData = getCityName();
+    alert("worked");
+  //  console.log(city);
+});
+
+$.getJSON('https://api.foursquare.com/v2/venues/search?intent=browse&near=64.8377778,-147.71638889999997&radius=19400&categoryId=4d4b7105d754a06374d81259&query=Coffee&client_id=ITCEQQH3TV4ARRVUCNLK1P4TW1XOM3S1RKIZVPHFY452HFQO&client_secret=SOHNSC0TT4XBM31HVZR4FT13M5JLUONTWWSXE3INES5Y3LUO&v=20130307',
     function(data) {
         $.each(data.response.venues, function(i,venues){
-
             content = '<p>' + venues.name +'</br>'+venues.location.formattedAddress+ '</br>'+
             venues.url+ '</br>' + venues.contact.formattedPhone+ '</br>'+ "Checkins Counted= "+ venues.stats.checkinsCount+  '</p>';
             $(content).appendTo("#names");
             console.log(data.response);
+       }); });
 
 
-       });
 
-});
 });
